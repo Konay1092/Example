1545e1edcb01541dfc03caf88a69882ae38db346
Konay812020 <nayoo812020@gmail.com>
2022-05-24 13:00:25 +0630

    d1

---
+++ Dataland/l5documentobject.js
@@ -0,0 +1,12 @@
+let val;
+
+val = document;
+val = document.doctype;
+val = document.body;
+val = document.head;
+val = document.URL;
+val = document.links; //HTMLCollectin[]
+val = document.links[0].className;
+val = document.links[0].classList;
+
+console.log(val);
